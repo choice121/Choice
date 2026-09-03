491e3184129c82b5b3bb5fbf247275a402c13cb9
choice121 <choice121@users.noreply.github.com>
2026-09-03 19:25:22 +0000

    Fix missing Link import in ListingsPage

--- frontend/src/pages/ListingsPage.tsx
+++ frontend/src/pages/ListingsPage.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Navbar } from '../components/Navbar'
 import { Footer } from '../components/Footer'
 import { PropertyList } from '../components/PropertyList'
