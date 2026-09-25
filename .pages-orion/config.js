72790ec0829d28ec5c7da7b3080e859f5a289df1
Choiceproperties-bit <gregoryhawkins217@gmail.com>
2026-09-25 21:46:33 +0000

    fix(orion): harden mobile extension workflow

--- .pages-orion/config.js
+++ .pages-orion/config.js
@@ -8,5 +8,5 @@
 window.CP_CONFIG = {
   IMPORT_SECRET: 'cp_import_7Kx3m9P2w5',
   EDGE_URL: 'https://tlfmwetmhthpyrytrcfo.supabase.co/functions/v1/receive-pipeline-import',
-  VERSION: '4.0.0'
+  VERSION: '18.0.6'
 };
